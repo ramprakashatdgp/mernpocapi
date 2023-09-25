82140978956419e80950aebaa8af29a3a97cb4fa
Ram Tiwary <ram.10723148@ltimindtree.com>
2023-09-25 15:55:38 +0530

    firstcommit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const colors = require("colors");
 const connectDb = require("./config/db");
-
+const path=require("path")
 //mongodb func call
 connectDb();
 
@@ -30,7 +30,11 @@ app.get("/", (req, res) => {
 
 //port number
 const PORT = 5000;
+app.use(express.static("./client/build"));
+app.get('*',(req,res)=>{
+res.sendFile(path.resolve(__dirname,"client","build","index.html"))
 
+})
 //listener
 app.listen(PORT, () => {
   console.log(`Server Running on Port = ${PORT}`.bgCyan);

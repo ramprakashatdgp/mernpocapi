cfa0214aa9157a647c0fc91bbcb24b0591d7ad8b
Ram Tiwary <ram.10723148@ltimindtree.com>
2023-09-27 17:25:46 +0530

    db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const colors = require("colors");
 
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://0.0.0.0:27017/POC");
+    const conn = await mongoose.connect("mongodb://mockapidb:9Goqje7rLxQRkdMMl1AakrVfgLy9nUz7HBk5sCnKqDjCEI5g8MjOuZnZl9lzVYbkA2WZGx7Qr9Y9ACDbBJWaVw==@mockapidb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@mockapidb@");
     console.log(`Connected to mongodb ${conn.connection.host}`.bgGreen);
   } catch (error) {
     console.log(`Mongodb Errro ${error.message}`.bgRed);
